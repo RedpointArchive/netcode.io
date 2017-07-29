fd93c340ca3689371b26a12ca99a3fc8ce2dd39c
June Rhodes <jrhodes@redpointgames.com.au>
2017-07-29 16:55:03 +1000

    Patch netcode.c so that it works on MacOS

--- Build/PreBuildMacOS.sh
+++ Build/PreBuildMacOS.sh
@@ -3,6 +3,10 @@
 set -e
 set -x
 
+# Patch 4MB limit to 2MB, as MacOS's limit for buffers is lower.
+alias replace='ruby -e "File.write(ARGV[0], File.read(ARGV[0]).gsub(ARGV[1], ARGV[2]))"'
+replace netcode.io-import/c/netcode.c "4 * 1024 * 1024" "2 * 1024 * 1024"
+
 pushd netcode.io-import
 export LIBS="-lsodium -L/usr/local/lib"
 cd c
