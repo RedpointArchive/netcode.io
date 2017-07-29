f864c212bf66c630a6c35f08d84755d2c68a6afd
June Rhodes <jrhodes@redpointgames.com.au>
2017-07-29 16:07:23 +1000

    Target .NET Standard 2.0 and copy .NET Core files post-build on Windows

--- Build/PostBuildWindows.ps1
+++ Build/PostBuildWindows.ps1
@@ -13,6 +13,14 @@ $ErrorActionPreference = 'Stop'
 cd $PSScriptRoot\..
 $root = Get-Location
 
+pushd netcode.io-csharp-core
+& "C:\Program Files\dotnet\dotnet.exe" build -c Release
+if ($LastExitCode -ne 0) {
+  popd
+  exit 1
+}
+popd
+
 & "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" /m /p:Platform=Win32 /p:Configuration=Release netcode.io\netcode.Windows.vcxproj
 if ($LastExitCode -ne 0) {
   exit 1
@@ -21,10 +29,12 @@ Copy-Item -Force netcode.io\bin\Windows\Win32\Release\netcode.dll netcode.io-csh
 Copy-Item -Force netcode.io\bin\Windows\Win32\Release\netcode.dll netcode.io.test\bin\Windows\AnyCPU\Release\netcode32.dll
 Copy-Item -Force netcode.io\bin\Windows\Win32\Release\netcode.dll netcode.io-client-example\bin\Windows\AnyCPU\Release\netcode32.dll
 Copy-Item -Force netcode.io\bin\Windows\Win32\Release\netcode.dll netcode.io-server-example\bin\Windows\AnyCPU\Release\netcode32.dll
+Copy-Item -Force netcode.io\bin\Windows\Win32\Release\netcode.dll netcode.io-csharp-core\bin\Release\netstandard2.0\netcode32.dll
 Copy-Item -Force netcode.io\bin\Windows\x64\Release\netcode.dll netcode.io-csharp\bin\Windows\AnyCPU\Release\netcode64.dll
 Copy-Item -Force netcode.io\bin\Windows\x64\Release\netcode.dll netcode.io.test\bin\Windows\AnyCPU\Release\netcode64.dll
 Copy-Item -Force netcode.io\bin\Windows\x64\Release\netcode.dll netcode.io-client-example\bin\Windows\AnyCPU\Release\netcode64.dll
 Copy-Item -Force netcode.io\bin\Windows\x64\Release\netcode.dll netcode.io-server-example\bin\Windows\AnyCPU\Release\netcode64.dll
+Copy-Item -Force netcode.io\bin\Windows\x64\Release\netcode.dll netcode.io-csharp-core\bin\Release\netstandard2.0\netcode64.dll
 
 if (!(Test-Path native-dist)) {
   mkdir native-dist
