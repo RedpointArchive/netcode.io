9218892176299eaa8dd71d3506379d1cc45e6e94
June Rhodes <jrhodes@redpointgames.com.au>
2017-07-29 17:30:12 +1000

    Install SWIG as well

--- Build/PreBuildLinux.sh
+++ Build/PreBuildLinux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Try to install libsodium for Linux
-apt-get install -y libsodium-dev libsodium18 || true
+apt-get install -y libsodium-dev libsodium18 swig || true
 
 pushd netcode.io-import
 cd c
